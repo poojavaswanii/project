6b802e9fe2542b4c33008569adf12b5bb682c728
poojavaswanii <69523733+poojavaswanii@users.noreply.github.com>
2021-05-16 20:25:32 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
  <footer class="foo">
             <div class="row">
                 <div class="col-lg-12">
-                        <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:30px;"><i class="material-icons" style="color: brown;"><b >ONLINE NOTES SHARING </b><b> - HMI PROJECT</b></p>
+                        <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:30px;"><i class="material-icons" style="color: brown;"><b >ONLINE NOTES SHARING </b><b> - CCL PROJECT</b></p>
                 </div>
             </div>
    </footer>
